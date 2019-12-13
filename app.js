da9696e03e997c43e03d9e41ef6d7ccbf759c9ca
David Silva <davidsilva2841+accounts+gitthub@gmail.com>
2019-12-12 18:10:50 -0600

    Added new route for testing

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'public')));
     app.use('/products', express.static(path.join(__dirname, 'public')));
     app.use('/products/:id?', express.static(path.join(__dirname, 'public')));
+    app.use('/testing-image-feature/:id?', express.static(path.join(__dirname, 'testing/image_feature/')));
+
     // app.get('/products/:id', express.static(path.join(__dirname, 'public')));
 } else if (process.env.NODE_ENV === 'development') {
     app.use('/', express.static(path.join(__dirname, 'public_testing')));
